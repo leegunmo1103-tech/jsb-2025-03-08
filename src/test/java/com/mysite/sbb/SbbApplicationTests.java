dd37268fb443c988d57d33c461a070f67df28a36
geonmo lee <leegunmo1103@gmail.com>
2026-03-08 22:49:01 +0900

    t1

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,29 @@
 package com.mysite.sbb;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SbbApplicationTests {
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	@Test
-	void contextLoads() {
+	@DisplayName("질문조회")
+	void t1() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
