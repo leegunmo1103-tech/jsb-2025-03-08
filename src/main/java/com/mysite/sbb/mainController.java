1663323f730ad3a6af222362a6d4c2e7943c913e
geonmo lee <leegunmo1103@gmail.com>
2026-03-09 22:02:57 +0900

    2-10

--- src/main/java/com/mysite/sbb/mainController.java
+++ src/main/java/com/mysite/sbb/mainController.java
@@ -6,22 +6,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class mainController {
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+
     @GetMapping("/")
-    @ResponseBody
-    public int home() {
-        System.out.println("home");
-        return 22;
-    }
-
-    @GetMapping("/about")
-    @ResponseBody
-    public String about() {
-        System.out.println("about");
-        return "안녕하세요. 저는 홍길동입니다.";
-    }
-
-    public void contact() {
-        System.out.println("contact");
+    public String root() {
+        return "redirect:/question/list";
     }
 }
 
